a2493b0dce54c73ecd6f094c31e7beefdac5cff8
DESKTOP-T32AIN6\Mark <mark.corelate@gmail.com>
2016-11-15 23:34:06 +0000

    Started on MutantCharacterController. Started on MutantAnimatorController. Added OVR Utilities package. New scene 'Aircraft' to test out Unity Standard Assets vehicle.

---
+++ Assets/Scripts/Corelate/HaloGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HaloGenerator : MonoBehaviour {
+
+    public Vector3 endPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Mutant/MutantCharacterController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MutantCharacterController : MonoBehaviour {
+
+    float leftJoystickVertical;
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+
+        animator = GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        leftJoystickVertical = Input.GetAxis("LeftJoystickVertical");
+
+        animator.SetFloat("Speed", Mathf.Abs(leftJoystickVertical));
+	}
+}
